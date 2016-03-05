1876b21b42e3832bc47818802df5c63923d111eb
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-03-06 02:53:37 +0530

    indentaion and code optimization

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-Parse.Cloud.beforeSave(Parse.User, function (request, response) {
-    //check if user being saved has profile picture.
-    if (!request.object.has("profilePic")) {
-        Parse.Config.get().then(function(config) {
-            request.object.set("profilePic", config.get("defaultAvatar"));
-            response.success();
-        });
-    }
-    if (!request.object.has("address")) {
-        Parse.Config.get().then(function(config) {
-            request.object.set("address", "Address");
-            response.success();
-        });
-    }
-    if (!request.object.has("city")) {
-        Parse.Config.get().then(function(config) {
-            request.object.set("city", "City");
-            response.success();
-        });
-    }
-    if (!request.object.has("country")) {
-        Parse.Config.get().then(function(config) {
-            request.object.set("country","Country");
-            response.success();
-        });
-    } 
-    else {
-        response.success();
-    }
+Parse.Cloud.beforeSave(Parse.User, function(request, response) {
+  //check if user being saved has profile picture.
+  if (!request.object.has("profilePic")) {
+    Parse.Config.get().then(function(config) {
+      request.object.set("profilePic", config.get("defaultAvatar"));
+      response.success();
+    });
+  }
+  if (!request.object.has("address")) {
+    Parse.Config.get().then(function(config) {
+      request.object.set("address", "Address");
+      response.success();
+    });
+  }
+  if (!request.object.has("city")) {
+    Parse.Config.get().then(function(config) {
+      request.object.set("city", "City");
+      response.success();
+    });
+  }
+  if (!request.object.has("country")) {
+    Parse.Config.get().then(function(config) {
+      request.object.set("country", "Country");
+      response.success();
+    });
+  } else {
+    response.success();
+  }
 });
